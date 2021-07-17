2617f65ea5f0da3f46fa683823b8e294a7eb85aa
Markus5555 <pupoks123@gmail.com>
2021-07-17 22:19:40 +0300

    add script, js. add barney(not Alive) no sound - autoplay does not work

--- Bart Game/bg.js
+++ Bart Game/bg.js
@@ -1,6 +1,8 @@
+
 const bart = document.getElementById('bart');
 const hydrant = document.getElementById("hydrant");
 const barney = document.getElementById('barney')
+
 document.addEventListener("keydown", function (event) {
     jump()
 });
@@ -16,11 +18,19 @@ function jump() {
  let isAlive = setInterval(function (){
      let bartTop = parseInt(window.getComputedStyle(bart).getPropertyValue('top'));
      let hydrantleft = parseInt(window.getComputedStyle(hydrant).getPropertyValue('left'));
-     if (hydrantleft < 100 && hydrantleft > 0 && bartTop >= 340){
-         alert('GAME OVER!');
-     }let barneyleft = parseInt(window.getComputedStyle(barney).getPropertyValue('left'));
-     if (barneyleft < 100 && barneyleft > 0 && bartTop >= 340){
+     if (hydrantleft < 100 && hydrantleft > 0 && bartTop >= 340 ){
          alert('GAME OVER!');
      }
- }, 10)
 
+ }, 10);
+// setTimeout(function (){
+//     bart.classList.remove('jump');
+// }, 300);
+//
+// let isAlive = setInterval(function (){
+//     let bartTop = parseInt(window.getComputedStyle(bart).getPropertyValue('top'));
+//     let barneyleft = parseInt(window.getComputedStyle(barney).getPropertyValue('left'));
+//     if (barneyleft < 100 && barneyleft > 0 && bartTop >= 240){
+//         alert('GAME OVER!');
+//     }
+// }, 10);
