1b317aec14d26c0b6829b699b30ee73db2ba1a9b
Markus5555 <pupoks123@gmail.com>
2021-07-17 11:41:57 +0300

    add script, js. no sound no barney

---
+++ Bart Game/bg.js
@@ -0,0 +1,26 @@
+const bart = document.getElementById('bart');
+const hydrant = document.getElementById("hydrant");
+const barney = document.getElementById('barney')
+document.addEventListener("keydown", function (event) {
+    jump()
+});
+
+function jump() {
+    if (bart.classList != 'jump') {
+        bart.classList.add('jump');
+    }
+    setTimeout(function (){
+        bart.classList.remove('jump');
+    }, 300);
+}
+ let isAlive = setInterval(function (){
+     let bartTop = parseInt(window.getComputedStyle(bart).getPropertyValue('top'));
+     let hydrantleft = parseInt(window.getComputedStyle(hydrant).getPropertyValue('left'));
+     if (hydrantleft < 100 && hydrantleft > 0 && bartTop >= 340){
+         alert('GAME OVER!');
+     }let barneyleft = parseInt(window.getComputedStyle(barney).getPropertyValue('left'));
+     if (barneyleft < 100 && barneyleft > 0 && bartTop >= 340){
+         alert('GAME OVER!');
+     }
+ }, 10)
+
